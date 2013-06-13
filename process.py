7a86d86f4774ad67701a814b303f68dd5e99bbb4
Will Kurt <wckurt@gmail.com>
2013-06-13 00:12:59 -0700

    forgot a few files

--- process.py
+++ process.py
@@ -4,13 +4,13 @@ import csv
 
 def process():
     quartet_title_pattern = "==(.+)=="
-    verse_title_pattern = "__(.+)__"
+    stanza_title_pattern = "__(.+)__"
     quartet_title = "None"
-    verse_title = "None"
+    stanza_title = "None"
     with open('four_quartets.txt','r') as f:
         with open('four_quartets.csv','w') as edt:
             csv_writer = csv.writer(edt)
-            csv_writer.writerow(["quartet","verse","line"])
+            csv_writer.writerow(["quartet","stanza","line"])
             for l in f:
 				#if matches ==.*==
 				#   set quartet title
@@ -19,16 +19,16 @@ def process():
 					quartet_title = m.group(1)
 					print(quartet_title)
 				#if matches __.*__
-				#    set verse title
-				elif re.search(verse_title_pattern,l):
-					m = re.match(verse_title_pattern,l)
-					verse_title = m.group(1)
-					print(verse_title)
+				#    set stanza title
+				elif re.search(stanza_title_pattern,l):
+					m = re.match(stanza_title_pattern,l)
+					stanza_title = m.group(1)
+					print(stanza_title)
 				#quick check for 'not a blank line'
 				elif re.search("[a-zA-Z0-9]",l):
 					clean_line = l.strip()
 					print(clean_line)
-					csv_writer.writerow([quartet_title,verse_title,clean_line])
+					csv_writer.writerow([quartet_title,stanza_title,clean_line])
 				else:
 					print("blank!")
                 #
